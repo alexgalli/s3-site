39b1e7ac5933d1009873bb153b1874f746101d9b
Alex Galli <famousbirds@gmail.com>
2019-03-17 20:57:35 -0700

    push deploy stack

--- src/cf_deploy/push_deploy_stack.sh
+++ src/cf_deploy/push_deploy_stack.sh
@@ -2,7 +2,7 @@
 
 pushd `dirname $0` > /dev/null
 
-if [[ $# == 0 ]] || [[ $1 == "" ]] || [[ $2 == '' ]] || ; then
+if [[ $# == 0 ]] || [[ $1 == "" ]] || [[ $2 == '' ]]; then
     echo "Usage: push_deploy_stack.sh <siteName> <env> [--create]"
     exit 1
 fi
@@ -13,10 +13,10 @@ CREATE=$3
 
 DEPLOYSTACKNAME=$SITENAME-$ENV-deploy
 
-if [[ $3 == '--create']]; then
-    aws cloudformation create-stack --stack-name $DEPLOYSTACKNAME --template-body file://deploy.yml --parameters ParameterKey=Environment,ParameterValue=$ENV ParameterKey=SiteName,ParameterValue=$SiteName
+if [[ $CREATE == '--create' ]]; then
+    aws cloudformation create-stack --stack-name $DEPLOYSTACKNAME --template-body file://deploy.yml --parameters ParameterKey=Environment,ParameterValue=$ENV ParameterKey=SiteName,ParameterValue=$SITENAME
 else
-    aws cloudformation update-stack --stack-name $DEPLOYSTACKNAME --template-body file://deploy.yml --parameters ParameterKey=Environment,ParameterValue=$ENV ParameterKey=SiteName,ParameterValue=$SiteName
+    aws cloudformation update-stack --stack-name $DEPLOYSTACKNAME --template-body file://deploy.yml --parameters ParameterKey=Environment,ParameterValue=$ENV ParameterKey=SiteName,ParameterValue=$SITENAME
 fi
 
 popd > /dev/null
