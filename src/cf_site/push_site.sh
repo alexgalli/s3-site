b95abb84fe020f6c596e53b0775ae363feea5235
Alex Galli <famousbirds@gmail.com>
2019-03-17 22:26:44 -0700

    push the site

--- src/cf_site/push_site.sh
+++ src/cf_site/push_site.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-SITENAME=alexgalli
-SITEPATH=$PROJECTPATH/sites/$SITENAME/site/
-SITEURL=alexgalli.co
-
-if [[ $# != 1 ]]; then
-    echo "Usage: push-site.sh <env>"
+if [[ $# != 3 ]] || [[ $1 == "" ]] || [[ $2 == "" ]] || [[ $3 == "" ]]; then
+    echo "Usage: push-site.sh <siteUrl> <env> <filePath>"
     exit 1
 fi
 
+SITEURL=$1
+ENV=$2
+FILEPATH=$3
+
 SITEENV=$1
 
-if [[ $SITEENV != "prod" ]]; then
-    S3PATH=$SITEENV.$SITEURL
+if [[ $ENV != "prod" ]]; then
+    S3PATH=$ENV.$SITEURL
 else
     S3PATH=$SITEURL
 fi
 
-aws s3 sync --delete $SITEPATH s3://$S3PATH/
+aws s3 sync --delete $FILEPATH s3://$S3PATH/
